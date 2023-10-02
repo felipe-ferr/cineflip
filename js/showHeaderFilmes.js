c1626e6d475352887df9ea02a5e0f9b451900af3
felipe-ferr <felipedealbuquerque54@gmail.com>
2023-10-01 21:16:13 -0300

    javascript funcionando

--- js/showHeaderFilmes.js
+++ js/showHeaderFilmes.js
@@ -1,1 +1,32 @@
-function showBarbie(){}
+function showbarbie(){
+
+    var oppen = document.getElementById("oppenfilme");
+    var barbie = document.getElementById("barbiefilme");
+
+    if (barbie.style.display === "none"){
+        barbie.style.display = "flex";
+        oppen.style.display = "none";
+    }
+    else{
+        oppen.style.display = "none";
+        barbie.style.display = "flex";
+    }
+}
+
+function showoppenheimer(){
+
+    var oppen = document.getElementById("oppenfilme");
+    var barbie = document.getElementById("barbiefilme");
+
+    if (oppen.style.display === "none"){
+        oppen.style.display = "flex";
+        barbie.style.display = "none";
+    }
+    else{
+        barbie.style.display = "none";
+        oppen.style.display = "flex";
+    }
+}
+
+
+  
