5a281861678352c3db66634f5bd217aaa706b8bf
felipe-ferr <felipedealbuquerque54@gmail.com>
2023-10-15 03:18:43 -0300

    header mudanças botoes

--- js/showHeaderFilmes.js
+++ js/showHeaderFilmes.js
@@ -4,17 +4,20 @@ function showbarbie(){
     var barbie = document.getElementById("barbiefilme");
     var mario = document.getElementById("mariofilme");
     var freira = document.getElementById("freirafilme");
+    var paw = document.getElementById("pawfilme");
 
     if (barbie.style.display === "none"){
         barbie.style.display = "flex";
         oppen.style.display = "none";
         mario.style.display = "none";
         freira.style.display = "none";
+        paw.style.display = "none";
     }
     else{
         oppen.style.display = "none";
         mario.style.display = "none";
         freira.style.display = "none";
+        paw.style.display = "none";
         barbie.style.display = "flex";
     }
 }
@@ -25,17 +28,20 @@ function showoppenheimer(){
     var barbie = document.getElementById("barbiefilme");
     var mario = document.getElementById("mariofilme");
     var freira = document.getElementById("freirafilme");
+    var paw = document.getElementById("pawfilme");
 
     if (oppen.style.display === "none"){
         oppen.style.display = "flex";
         barbie.style.display = "none";
         mario.style.display = "none";
         freira.style.display = "none";
+        paw.style.display = "none";
     }
     else{
         barbie.style.display = "none";
         freira.style.display = "none";
         mario.style.display = "none";
+        paw.style.display = "none";
         oppen.style.display = "flex";
     }
 }
@@ -45,17 +51,20 @@ function showmario(){
     var barbie = document.getElementById("barbiefilme");
     var mario = document.getElementById("mariofilme");
     var freira = document.getElementById("freirafilme");
+    var paw = document.getElementById("pawfilme");
 
     if (mario.style.display === "none"){
         mario.style.display = "flex";
         barbie.style.display = "none";
         oppen.style.display = "none";
         freira.style.display = "none";
+        paw.style.display = "none";
     }
     else{
         barbie.style.display = "none";
         freira.style.display = "none";
         oppen.style.display = "none";
+        paw.style.display = "none";
         mario.style.display = "flex";
     }
 }
@@ -65,18 +74,44 @@ function showfreira(){
     var barbie = document.getElementById("barbiefilme");
     var mario = document.getElementById("mariofilme");
     var freira = document.getElementById("freirafilme");
+    var paw = document.getElementById("pawfilme");
 
     if (freira.style.display === "none"){
         freira.style.display = "flex";
         barbie.style.display = "none";
         oppen.style.display = "none";
         mario.style.display = "none";
+        paw.style.display = "none";
     }
     else{
         barbie.style.display = "none";
         mario.style.display = "none";
         oppen.style.display = "none";
+        paw.style.display = "none";
         freira.style.display = "flex";
+    }
+}
+function showpaw(){
+
+    var oppen = document.getElementById("oppenfilme");
+    var barbie = document.getElementById("barbiefilme");
+    var mario = document.getElementById("mariofilme");
+    var freira = document.getElementById("freirafilme");
+    var paw = document.getElementById("pawfilme");
+
+    if (paw.style.display === "none"){
+        paw.style.display = "flex";
+        barbie.style.display = "none";
+        mario.style.display = "none";
+        freira.style.display = "none";
+        oppen.style.display = "none";
+    }
+    else{
+        barbie.style.display = "none";
+        freira.style.display = "none";
+        mario.style.display = "none";
+        oppen.style.display = "none";
+        paw.style.display = "flex";
     }
 }
 
