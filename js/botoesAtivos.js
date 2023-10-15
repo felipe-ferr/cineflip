1a16de4037c4b2f8257078d2536ea24d29efe84b
felipe-ferr <felipedealbuquerque54@gmail.com>
2023-10-14 23:49:13 -0300

    js botoes ativos/ajuste header

--- js/botoesAtivos.js
+++ js/botoesAtivos.js
@@ -1,9 +1,14 @@
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+    // Identifica o container dos botões
+        var btnContainer = document.getElementById("myDIV");
+        
+        // Variável para os botôes com a classe "filme-poster"
+        var btns = btnContainer.getElementsByClassName("filme-poster");
+        
+        // For para adicionar a classe active para o botão ativo
+        for (var i = 0; i < btns.length; i++) {
+          btns[i].addEventListener("click", function() {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+          });
+        }
