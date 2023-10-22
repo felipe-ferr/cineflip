ac1c11bcd73c13c8cfd4203ea30a874ed49c8042
felipe-ferr <felipedealbuquerque54@gmail.com>
2023-10-22 20:46:24 -0300

    pagina ingresso metade pronta

---
+++ js/comprarIngressoItems.js
@@ -0,0 +1,24 @@
+function mostrarIdioma(){
+    var x = document.getElementById("mostraridioma");
+
+    x.style.display = "flex";
+
+}
+function mostrarAssentos(){
+        var x = document.getElementById("mostrarassentos");
+
+        x.style.display = "flex";
+
+}
+function mostrarHorarios(){
+    var x = document.getElementById("mostrarhorarios");
+
+    x.style.display = "flex";
+
+}
+function mostrarTipoingresso(){
+    var x = document.getElementById("mostrartipoingresso");
+
+    x.style.display = "flex";
+
+}
