3cbdcf62cbe0c1bf186b8c1735c8aad77755659f
felipe-ferr <felipedealbuquerque54@gmail.com>
2023-10-22 21:00:36 -0300

    colocar animações

--- js/comprarIngressoItems.js
+++ js/comprarIngressoItems.js
@@ -2,23 +2,29 @@ function mostrarIdioma(){
     var x = document.getElementById("mostraridioma");
 
     x.style.display = "flex";
+    document.getElementById("mostraridioma").scrollIntoView({behavior: "smooth"});
+    
+    
 
 }
 function mostrarAssentos(){
         var x = document.getElementById("mostrarassentos");
 
         x.style.display = "flex";
+        document.getElementById("mostrarassentos").scrollIntoView({behavior: "smooth"});
 
 }
 function mostrarHorarios(){
     var x = document.getElementById("mostrarhorarios");
 
     x.style.display = "flex";
+    document.getElementById("mostrarhorarios").scrollIntoView({behavior: "smooth"});
 
 }
 function mostrarTipoingresso(){
     var x = document.getElementById("mostrartipoingresso");
 
     x.style.display = "flex";
+    document.getElementById("mostrartipoingresso").scrollIntoView({behavior: "smooth"});
 
 }
