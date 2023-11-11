6df7ca9310ccfa62fecd05e97b35e4d74d3370d3
felipe-ferr <felipedealbuquerque54@gmail.com>
2023-11-11 13:24:08 -0300

    js resumo pedido começo

---
+++ js/resumo-pedido.js
@@ -0,0 +1,11 @@
+function mostrarNumero(){
+    input = document.getElementById("numeropipoca").value;
+
+    document.getElementById("numeropedido").innerHTML = input;
+
+}
+function mostrarNome(){
+    input = document.getElementById("titulopedido").title;
+
+    document.getElementById("nomepedido").innerHTML = input;
+}
