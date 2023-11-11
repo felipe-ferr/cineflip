27458ae045a63c0e257d8f6e33ba8713c1af94cc
felipe-ferr <felipedealbuquerque54@gmail.com>
2023-11-11 19:06:25 -0300

    javascript resumo pedido

--- js/resumo-pedido.js
+++ js/resumo-pedido.js
@@ -1,11 +1,52 @@
-function mostrarNumero(){
-    input = document.getElementById("numeropipoca").value;
+// função pedido pipoca simples
 
-    document.getElementById("numeropedido").innerHTML = input;
+function mostrarNumeroPipocaSimples(){
+    input = parseFloat(document.getElementById("numero-pipoca-simples").value);
 
+    document.getElementById("numero-pedido-pipoca-simples").innerHTML = input;
+    document.getElementById("pipoca-simples").style.display = "flex";
+
+    valor = parseFloat(document.getElementById("valor-pipoca-simples").title).toFixed(2);
+
+    valor = valor * input;
+    document.getElementById("valor-pedido-pipoca-simples").innerHTML = valor;
+
+
+    if (document.getElementById("numero-pipoca-simples").value < 1){
+        document.getElementById("pipoca-simples").style.display = "none";
+    }
 }
-function mostrarNome(){
-    input = document.getElementById("titulopedido").title;
 
-    document.getElementById("nomepedido").innerHTML = input;
+function mostrarNomePipocaSimples(){
+    pedido = document.getElementById("titulo-pedido-pipoca-simples").title;
+
+    document.getElementById("nome-pedido-pipoca-simples").innerHTML = pedido;
 }
+
+
+// função pedido pipoca caramelizada------------------------------------------------------------------
+
+function mostrarNumeroPipocaCaramelizada(){
+    input = parseFloat(document.getElementById("numero-pipoca-caramelizada").value);
+
+    document.getElementById("numero-pedido-pipoca-caramelizada").innerHTML = input;
+    document.getElementById("pipoca-caramelizada").style.display = "flex";
+
+    valor = parseFloat(document.getElementById("valor-pipoca-caramelizada").title).toFixed(2);
+
+    valor = valor * input;
+    document.getElementById("valor-pedido-pipoca-caramelizada").innerHTML = valor;
+
+
+    if (document.getElementById("numero-pipoca-caramelizada").value < 1){
+        document.getElementById("pipoca-caramelizada").style.display = "none";
+    }
+}
+
+function mostrarNomePipocaCaramelizada(){
+    pedido = document.getElementById("titulo-pedido-pipoca-caramelizada").title;
+
+    document.getElementById("nome-pedido-pipoca-caramelizada").innerHTML = pedido;
+}
+
+
--- js/showHeaderFilmes.js
+++ js/showHeaderFilmes.js
@@ -1,116 +1,61 @@
-function showbarbie() {
+var oppen = document.getElementById("oppenfilme");
+var barbie = document.getElementById("barbiefilme");
+var mario = document.getElementById("mariofilme");
+var freira = document.getElementById("freirafilme");
+var paw = document.getElementById("pawfilme");
 
-    var oppen = document.getElementById("oppenfilme");
-    var barbie = document.getElementById("barbiefilme");
-    var mario = document.getElementById("mariofilme");
-    var freira = document.getElementById("freirafilme");
-    var paw = document.getElementById("pawfilme");
+function showbarbie() {
 
     if (barbie.style.display === "none") {
         barbie.style.display = "flex";
-        oppen.style.display = "none";
-        mario.style.display = "none";
-        freira.style.display = "none";
-        paw.style.display = "none";
+        mario.style.display = oppen.style.display = freira.style.display = paw.style.display = "none";
     }
     else {
-        oppen.style.display = "none";
-        mario.style.display = "none";
-        freira.style.display = "none";
-        paw.style.display = "none";
+        mario.style.display = oppen.style.display = freira.style.display = paw.style.display = "none";
         barbie.style.display = "flex";
     }
 }
 
 function showoppenheimer() {
 
-    var oppen = document.getElementById("oppenfilme");
-    var barbie = document.getElementById("barbiefilme");
-    var mario = document.getElementById("mariofilme");
-    var freira = document.getElementById("freirafilme");
-    var paw = document.getElementById("pawfilme");
-
     if (oppen.style.display === "none") {
-        oppen.style.display = "flex";
-        barbie.style.display = "none";
-        mario.style.display = "none";
-        freira.style.display = "none";
-        paw.style.display = "none";
+        mario.style.display = barbie.style.display = freira.style.display = paw.style.display = "none";
     }
     else {
-        barbie.style.display = "none";
-        freira.style.display = "none";
-        mario.style.display = "none";
-        paw.style.display = "none";
+        mario.style.display = barbie.style.display = freira.style.display = paw.style.display = "none";
         oppen.style.display = "flex";
     }
 }
 function showmario() {
 
-    var oppen = document.getElementById("oppenfilme");
-    var barbie = document.getElementById("barbiefilme");
-    var mario = document.getElementById("mariofilme");
-    var freira = document.getElementById("freirafilme");
-    var paw = document.getElementById("pawfilme");
-
     if (mario.style.display === "none") {
         mario.style.display = "flex";
-        barbie.style.display = "none";
-        oppen.style.display = "none";
-        freira.style.display = "none";
-        paw.style.display = "none";
+        barbie.style.display = oppen.style.display = freira.style.display = paw.style.display = "none";
     }
     else {
-        barbie.style.display = "none";
-        freira.style.display = "none";
-        oppen.style.display = "none";
-        paw.style.display = "none";
+        barbie.style.display = oppen.style.display = freira.style.display = paw.style.display = "none";
         mario.style.display = "flex";
     }
 }
 function showfreira() {
 
-    var oppen = document.getElementById("oppenfilme");
-    var barbie = document.getElementById("barbiefilme");
-    var mario = document.getElementById("mariofilme");
-    var freira = document.getElementById("freirafilme");
-    var paw = document.getElementById("pawfilme");
-
     if (freira.style.display === "none") {
         freira.style.display = "flex";
-        barbie.style.display = "none";
-        oppen.style.display = "none";
-        mario.style.display = "none";
-        paw.style.display = "none";
+        mario.style.display = oppen.style.display = barbie.style.display = paw.style.display = "none";
     }
     else {
-        barbie.style.display = "none";
-        mario.style.display = "none";
-        oppen.style.display = "none";
-        paw.style.display = "none";
+        mario.style.display = oppen.style.display = barbie.style.display = paw.style.display = "none";
         freira.style.display = "flex";
     }
 }
 function showpaw() {
 
-    var oppen = document.getElementById("oppenfilme");
-    var barbie = document.getElementById("barbiefilme");
-    var mario = document.getElementById("mariofilme");
-    var freira = document.getElementById("freirafilme");
-    var paw = document.getElementById("pawfilme");
-
     if (paw.style.display === "none") {
         paw.style.display = "flex";
-        barbie.style.display = "none";
-        mario.style.display = "none";
-        freira.style.display = "none";
-        oppen.style.display = "none";
+        mario.style.display = oppen.style.display = freira.style.display = barbie.style.display = "none";
     }
     else {
-        barbie.style.display = "none";
-        freira.style.display = "none";
-        mario.style.display = "none";
-        oppen.style.display = "none";
+        mario.style.display = oppen.style.display = freira.style.display = barbie.style.display = "none";
         paw.style.display = "flex";
     }
 }
